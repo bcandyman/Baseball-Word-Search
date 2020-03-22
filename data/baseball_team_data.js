6459e461aaee158fd8ffb05a516f221ae1ffa655
Benjamin Candy <bcandyman93@gmail.com>
2020-03-22 15:25:10 -0700

    Changed location in which logos are pulled.

--- data/baseball_team_data.js
+++ data/baseball_team_data.js
@@ -2,210 +2,209 @@ mlb_data = {
   Diamondbacks: {
     location: "Arizona",
     city: "Phoenix",
-    logo_url: "https://en.wikipedia.org/wiki/Arizona_Diamondbacks#/media/File:Arizona_Diamondbacks_logo_(low_res).svg",
+    logo_url: "https://content.sportslogos.net/logos/54/50/full/gnnnrbxcmjhdgeu6mavqk3945.png",
     mlb_org: "Arizona Diamondbacks",
     name: "Diamondbacks"
   },
   Braves: {
     location: "Atlanta",
     city: "Atlanta",
-    logo_url: "https://en.wikipedia.org/wiki/Atlanta_Braves#/media/File:Atlanta_Braves.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/51/full/7150_atlanta_braves-primary-2018.png",
     mlb_org: "Atlanta Braves",
     name: "Braves"
   },
   Orioles: {
     location: "Baltimore",
     city: "Baltimore",
-    logo_url: "https://en.wikipedia.org/wiki/Baltimore_Orioles#/media/File:Baltimore_Orioles_cap.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/52/full/5800_baltimore_orioles-primary-2019.png",
     mlb_org: "Baltimore Orioles",
     name: "Orioles"
   },
   Red_Sox: {
     location: "Boston",
     city: "Boston",
-    logo_url: "https://en.wikipedia.org/wiki/Boston_Red_Sox#/media/File:RedSoxPrimary_HangingSocks.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/53/full/c0whfsa9j0vbs079opk2s05lx.png",
     mlb_org: "Boston Red Sox",
     name: "Red Sox"
   },
   Cubs: {
     location: "Chicago",
     city: "Chicago",
-    logo_url: "https://en.wikipedia.org/wiki/Chicago_Cubs#/media/File:Chicago_Cubs_logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/54/full/q9gvs07u72gc9xr3395u6jh68.png",
     mlb_org: "Chicago Cubs",
     name: "Cubs"
   },
   White_Sox: {
     location: "Chicago",
     city: "Chicago",
-    logo_url: "https://en.wikipedia.org/wiki/Chicago_White_Sox#/media/File:Chicago_White_Sox.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/55/full/oxvkprv7v4inf5dgqdebp0yse.png",
     mlb_org: "Chicago White Sox",
     name: "White Sox"
   },
   Reds: {
     location: "Cincinnati",
     city: "Cincinnati",
-    logo_url: "https://en.wikipedia.org/wiki/Cincinnati_Reds#/media/File:Cincinnati_Reds_Logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/56/full/z9e0rqit393ojiizsemd0t1hx.png",
     mlb_org: "Cincinnati Reds",
     name: "Reds"
   },
   Indians: {
     location: "Cleveland",
     city: "Cleveland",
-    logo_url: "https://en.wikipedia.org/wiki/Cleveland_Indians#/media/File:Cleveland_Indians_primary_logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/57/full/5347_cleveland_indians-primary-2014.png",
     mlb_org: "Cleveland Indians",
     name: "Indians"
   },
   Rockies: {
     location: "Colorado",
     city: "Denver",
-    logo_url: "https://en.wikipedia.org/wiki/Colorado_Rockies#/media/File:Colorado_Rockies_logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/58/full/6871_colorado_rockies-primary-2017.png",
     mlb_org: "Colorado Rockies",
     name: "Rockies"
   },
   Tigers: {
     location: "Detroit",
     city: "Detroit",
-    logo_url: "https://en.wikipedia.org/wiki/Detroit_Tigers#/media/File:Detroit_Tigers_logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/59/full/8964_detroit_tigers-primary-2016.png",
     mlb_org: "Detroit Tigers",
     name: "Tigers"
   },
   Astros: {
     location: "Houston",
     city: "Houston",
-    logo_url: "https://en.wikipedia.org/wiki/Houston_Astros#/media/File:Houston-Astros-Logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/4929/full/9503_houston_astros-primary-2000.png",
     mlb_org: "Houston Astros",
     name: "Astros"
   },
   Royals: {
     location: "Kansas City",
     city: "Kansas City",
-    logo_url: "https://en.wikipedia.org/wiki/Kansas_City_Royals#/media/File:Kansas_City_Royals.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/62/full/8322_kansas_city_royals-primary-2019.png",
     mlb_org: "Kansas City Royals",
     name: "Royals"
   },
   Angels: {
     location: "Los Angeles",
     city: "Anaheim",
-    logo_url: "https://en.wikipedia.org/wiki/Los_Angeles_Angels#/media/File:Los_Angeles_Angels_of_Anaheim.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/6521/full/4389_los_angeles_angels-primary-2016.png",
     mlb_org: "Los Angeles Angels",
     name: "Angels"
   },
   Dodgers: {
     location: "Los Angeles",
     city: "Los Angeles",
-    logo_url: "https://en.wikipedia.org/wiki/Los_Angeles_Dodgers#/media/File:Los_Angeles_Dodgers_logo_(low_res).svg",
+    logo_url: "https://content.sportslogos.net/logos/54/63/full/efvfv5b5g1zgpsf56gb04lthx.png",
     mlb_org: "Los Angeles Dodgers",
     name: "Dodgers"
   },
   Marlins: {
     location: "Miami",
     city: "Miami",
-    logo_url: "",
-    mlb_org: "Miami Marlins",
+    logo_url: "https://content.sportslogos.net/logos/54/3637/full/7544_miami_marlins-primary-2019.png",
     name: "Marlins"
   },
   Brewers: {
     location: "Milwaukee",
     city: "Milwaukee",
-    logo_url: "https://en.wikipedia.org/wiki/Miami_Marlins#/media/File:Marlins_team_logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/64/full/7997_milwaukee_brewers-primary-2018.png",
     mlb_org: "Milwaukee Brewers",
     name: "Brewers"
   },
   Twins: {
     location: "Minnesota",
     city: "Minneapolis",
-    logo_url: "https://en.wikipedia.org/wiki/Minnesota_Twins#/media/File:Minnesota_Twins_logo_(low_res).svg",
+    logo_url: "https://content.sportslogos.net/logos/53/65/full/peii986yf4l42v3aa3hy0ovlf.png",
     mlb_org: "Minnesota Twins",
     name: "Twins"
   },
   Mets: {
     location: "New York",
     city: "New York",
-    logo_url: "https://en.wikipedia.org/wiki/New_York_Mets#/media/File:New_York_Mets.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/67/full/m01gfgeorgvbfw15fy04alujm.png",
     mlb_org: "New York Mets",
     name: "Mets"
   },
   Yankees: {
     location: "New York",
     city: "Bronx",
-    logo_url: "https://en.wikipedia.org/wiki/New_York_Yankees#/media/File:NewYorkYankees_PrimaryLogo.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/68/full/1256.png",
     mlb_org: "New York Yankees",
     name: "Yankees"
   },
   Athletics: {
     location: "Oakland",
     city: "Oakland",
-    logo_url: "https://en.wikipedia.org/wiki/Oakland_Athletics#/media/File:Oakland_A's_logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/69/full/6xk2lpc36146pbg2kydf13e50.png",
     mlb_org: "Oakland Athletics",
     name: "Athletics"
   },
   Phillies: {
     location: "Philadelphia",
     city: "Philadelphia",
-    logo_url: "https://en.wikipedia.org/wiki/Philadelphia_Phillies#/media/File:Philadelphia_Phillies_(2019)_logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/70/full/7579_philadelphia_phillies-primary-2019.png",
     mlb_org: "Philadelphia Phillies",
     name: "Phillies"
   },
   Pirates: {
     location: "Pittsburgh",
     city: "Pittsburgh",
-    logo_url: "https://en.wikipedia.org/wiki/Pittsburgh_Pirates#/media/File:Pittsburgh_Pirates_logo_2014.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/71/full/1250_pittsburgh_pirates-primary-2014.png",
     mlb_org: "Pittsburgh Pirates",
     name: "Pirates"
   },
   Padres: {
     location: "San Diego",
     city: "San Diego",
-    logo_url: "https://en.wikipedia.org/wiki/San_Diego_Padres#/media/File:SD_Logo_Brown.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/73/full/4344_san_diego_padres-primary-2015.png",
     mlb_org: "San Diego Padres",
     name: "Padres"
   },
   Giants: {
     location: "San Francisco",
     city: "San Francisco",
-    logo_url: "https://en.wikipedia.org/wiki/San_Francisco_Giants#/media/File:San_Francisco_Giants_Logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/74/full/cpqj6up5bvgpoedg5fwsk20ve.png",
     mlb_org: "San Francisco Giants",
     name: "Giants"
   },
   Mariners: {
     location: "Seattle",
     city: "Seattle",
-    logo_url: "https://en.wikipedia.org/wiki/Seattle_Mariners#/media/File:Seattle_Mariners_logo_(low_res).svg",
+    logo_url: "https://content.sportslogos.net/logos/53/75/full/1305.png",
     mlb_org: "Seattle Mariners",
     name: "Mariners"
   },
   Cardinals: {
     location: "St. Louis",
     city: "St. Louis",
-    logo_url: "https://en.wikipedia.org/wiki/St._Louis_Cardinals#/media/File:St._Louis_Cardinals_logo.svg",
+    logo_url: "https://content.sportslogos.net/logos/54/72/full/3zhma0aeq17tktge1huh7yok5.png",
     mlb_org: "St. Louis Cardinals",
     name: "Cardinals"
   },
   Rays: {
     location: "Tampa Bay",
     city: "Tampa Bay",
-    logo_url: "https://en.wikipedia.org/wiki/Tampa_Bay_Rays#/media/File:Tampa_Bay_Rays.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/2535/full/9448_tampa_bay_rays-primary-2019.png",
     mlb_org: "Tampa Bay Rays",
     name: "Rays"
   },
   Rangers: {
     location: "Texas",
     city: "Arlington",
-    logo_url: "https://en.wikipedia.org/wiki/Texas_Rangers_(baseball)#/media/File:Texas_Rangers.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/77/full/ajfeh4oqeealq37er15r3673h.png",
     mlb_org: "Texas Rangers",
     name: "Rangers"
   },
   Blue_Jays: {
     location: "Toronto",
     city: "Toronto",
-    logo_url: "https://en.wikipedia.org/wiki/Toronto_Blue_Jays#/media/File:Blue_Jays.svg",
+    logo_url: "https://content.sportslogos.net/logos/53/78/full/2559d7603ouedg7ldhw0br4fn.png",
     mlb_org: "Toronto Blue Jays",
     name: "Blue Jays"
   },
   Nationals: {
     location: "Washington",
     city: "Washington",
-    logo_url: "https://en.wikipedia.org/wiki/Washington_Nationals#/media/File:Washington_Nationals_logo_(low_res).svg",
+    logo_url: "https://content.sportslogos.net/logos/54/578/full/rcehah9k0kekjkgzm077fflws.png",
     mlb_org: "Washington Nationals",
     name: "Nationals"
   }
